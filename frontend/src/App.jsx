5f9fb78503a3fc06e6c2f2c7413fd17c5e014a48
KlonChannel <nikita20040521@gmail.com>
2025-07-31 21:36:35 +0300

    Create Registration window and adaptive layout for them and login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 import './App.css';
 
 import Login from './components/Login/Login';
+import Registration from './components/Registration/Registration';
 
 const App = () => {
   return (
     <div className="App">
       <main>
-        <Login />
+        <Registration />
       </main>
     </div>
   );
